8bf4192ea6d3bdf647463f418ae76e1c55e74d2d
vasyl_chechelnisky <vasyl_chechelnisky@granddevs.com>
2021-09-22 17:58:41 +0300

    - created Scripts folder and MainMenuController script

---
+++ New Unity Project/Assets/Scripts/MainMenuController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenuController : MonoBehaviour
+{
+    [SerializeField] private Button startButton; 
+    [SerializeField] private string sceneName;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startButton.onClick.AddListener(StartGameScene);
+    }
+
+    public void StartGameScene()
+    {
+        SceneManager.LoadSceneAsync(sceneName, LoadSceneMode.Additive);
+        startButton.onClick.RemoveAllListeners();
+    }
+}
