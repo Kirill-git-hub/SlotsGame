8234cb9db326c47ef4bece86914cb55491195efc
Kirill_Vdovin <kirilvdovinkv@gmail.com>
2021-09-23 16:28:55 +0300

    - added SceneLoader script

---
+++ New Unity Project/Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    [SerializeField] private List<string> scenes;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        foreach(string sceneName in scenes)
+        {
+            SceneManager.LoadSceneAsync(sceneName, LoadSceneMode.Additive);
+        }
+    }
+}
