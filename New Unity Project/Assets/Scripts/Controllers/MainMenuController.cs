c5aed5ad05798cf3892f9b3554ebc30dd21baebd
Kirill_Vdovin <kirilvdovinkv@gmail.com>
2021-09-27 10:00:11 +0300

    - created MainMenuController script

---
+++ New Unity Project/Assets/Scripts/Controllers/MainMenuController.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenuController : MonoBehaviour
+{
+    private MainMenuView mainMenuView;
+
+    public MainMenuView MainMenuView { get => mainMenuView; set => mainMenuView = value; }
+}
