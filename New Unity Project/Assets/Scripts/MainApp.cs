d1c85cda81ab1f65084995fd866a22ccea898e79
Kirill_Vdovin <kirilvdovinkv@gmail.com>
2021-09-27 10:02:20 +0300

    - created MainApp script

---
+++ New Unity Project/Assets/Scripts/MainApp.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainApp : MonoBehaviour
+{
+    public static MainApp instance = null;
+
+    private void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this;
+        }
+        else if (instance == this)
+        {
+            Destroy(gameObject);
+        }
+
+        DontDestroyOnLoad(gameObject);
+    }
+
+    private MainMenuController mainMenuController;
+
+    public MainMenuController MainMenuController { get => mainMenuController; set => mainMenuController = value; }
+}
