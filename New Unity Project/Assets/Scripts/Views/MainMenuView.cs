6bce4b71c82419df543fae873c825d730bd9eacf
Kirill_Vdovin <kirilvdovinkv@gmail.com>
2021-09-27 10:03:11 +0300

    - created MainMenuView

---
+++ New Unity Project/Assets/Scripts/Views/MainMenuView.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MainMenuView : MonoBehaviour
+{
+    [SerializeField] private Button startGameButton;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startGameButton.onClick.AddListener(InitializiGameScene);
+    }
+
+    public void InitializiGameScene()
+    {
+        SceneManager.LoadScene("GamePlay");
+    }
+
+}
