27feddc92c52bd45b0d58d74581607bf2b937fda
Kirill_Vdovin <kirilvdovinkv@gmail.com>
2021-09-28 09:23:24 +0300

    - added listener on pauseButton to invoke pause panel

---
+++ New Unity Project/Assets/Scripts/Views/GameView.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameView : MonoBehaviour
+{
+    [SerializeField] private Button pauseButton;
+
+    private void Start()
+    {
+        pauseButton.onClick.AddListener(PopupController.instance.InstantiatePausePopup);
+        Debug.Log("прикрепил ивент активировать панель паузы");
+    }
+}
