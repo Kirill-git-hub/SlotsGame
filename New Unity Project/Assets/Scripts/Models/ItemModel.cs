e50be3bdad40fa1fcb52abfca8c61547efc75e2e
Kirill_Vdovin <kirilvdovinkv@gmail.com>
2021-10-06 10:47:33 +0300

    - added method to initialize sprites

--- New Unity Project/Assets/Scripts/Models/ItemModel.cs
+++ New Unity Project/Assets/Scripts/Models/ItemModel.cs
@@ -1,10 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ItemModel : MonoBehaviour
 {
-    [SerializeField] private Sprite slotSprite;
+    [SerializeField] private Image itemImage = null;
     [SerializeField] private ItemType slotType;
     [SerializeField] private float payout;
+
+    public void Init(Sprite sprite)
+    {
+        itemImage.sprite = sprite;
+    }
 }
