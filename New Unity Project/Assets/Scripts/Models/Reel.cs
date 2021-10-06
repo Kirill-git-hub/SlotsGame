5b9ae472f0825154f6f396b16c8038d66cea6284
Kirill_Vdovin <kirilvdovinkv@gmail.com>
2021-10-06 10:37:38 +0300

    - created Reel script

---
+++ New Unity Project/Assets/Scripts/Models/Reel.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Reel : MonoBehaviour
+{
+
+    [SerializeField] private List<ItemModel> itemsList = new List<ItemModel>();
+    [SerializeField] private List<Sprite> spritesList = new List<Sprite>();
+    [SerializeField] private float delay;
+    [SerializeField] private float speed;
+
+    private Vector2 startPos;
+
+    public Vector2 StartPos { get => startPos; set => startPos = value; }
+    public float Speed { get => speed; set => speed = value; }
+
+    private void Start()
+    {
+        //startPos = transform.position;
+        SetItems();
+    }
+
+    private void Update()
+    {
+        //RotateReel();
+    }
+
+    public void RotateReel(float reelSpeed)
+    {
+        transform.Translate(Vector2.down * Time.deltaTime * reelSpeed);
+    }
+
+    public void SetItems()
+    {
+        foreach (ItemModel item in itemsList)
+        {
+            item.Init(spritesList[Random.Range(0, spritesList.Count)]);
+        }
+    }
+}
