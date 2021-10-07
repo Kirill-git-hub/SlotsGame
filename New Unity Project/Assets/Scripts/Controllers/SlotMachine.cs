55cbc0f1fb1fd76ab3ab2b06601415ef2e9b18c0
Kirill_Vdovin <kirilvdovinkv@gmail.com>
2021-10-07 13:05:57 +0300

    - added methods to instantiate reels at their positions

--- New Unity Project/Assets/Scripts/Controllers/SlotMachine.cs
+++ New Unity Project/Assets/Scripts/Controllers/SlotMachine.cs
@@ -5,13 +5,15 @@ using UnityEngine;
 public class SlotMachine : MonoBehaviour
 {
     //[SerializeField] private Reel[] reelModels;
-
-    [SerializeField] private GameObject[] reelSet;
+    [SerializeField] private GameObject[] reelContainer = null;
+    [SerializeField] private Reel reelPrefab = null;
 
     // Start is called before the first frame update
     void Start()
     {
         MainApp.instance.GameController.SlotMachine = this;
+
+        InstantiateReelsAtFinalPos();
     }
 
     // Update is called once per frame
@@ -20,17 +22,42 @@ public class SlotMachine : MonoBehaviour
         StartRotating();
     }
 
+    public void InstantiateReelsAtFinalPos()
+    {
+        for (int i = 0; i < reelContainer.Length; i++)
+        {
+            Reel reel = Instantiate(reelPrefab, reelContainer[i].transform);
+
+            SetReelPosition(reel, reel.FinalPos);
+        }
+    }
+
+    public void InstantiateReelsAtStartingPos()
+    {
+        for (int i = 0; i < reelContainer.Length; i++)
+        {
+            Reel reel = Instantiate(reelPrefab, reelContainer[i].transform);
+
+            SetReelPosition(reel, reel.StartingPos);
+        }
+    }
+
+    private void SetReelPosition(Reel reel, Vector3 currentPos)
+    {
+        reel.SetPosition(currentPos);
+    }
+
     public void StartRotating()
     {
         //StartCoroutine(RotateNextReel());
-        for (int i = 0; i < reelSet.Length; i++)
-        {
-            for (int j = 0; j < reelSet[i].transform.childCount; j++)
-            {
-                float speed = reelSet[i].transform.GetChild(j).GetComponent<Reel>().Speed;
-                reelSet[i].transform.GetChild(j).GetComponent<Reel>().RotateReel(speed);
-            }
-        }
+        //for (int i = 0; i < reelSet.Length; i++)
+        //{
+        //    for (int j = 0; j < reelSet[i].transform.childCount; j++)
+        //    {
+        //        float speed = reelSet[i].transform.GetChild(j).GetComponent<Reel>().Speed;
+        //        reelSet[i].transform.GetChild(j).GetComponent<Reel>().RotateReel(speed);
+        //    }
+        //}
     }
 
     //public IEnumerator RotateNextReel()
