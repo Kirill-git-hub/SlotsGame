75a663a2cc180a884dca56025c29a7365a415dcf
Kirill_Vdovin <kirilvdovinkv@gmail.com>
2021-10-07 13:03:37 +0300

    - added a new variables of positions and method to set position

--- New Unity Project/Assets/Scripts/Models/Reel.cs
+++ New Unity Project/Assets/Scripts/Models/Reel.cs
@@ -9,17 +9,26 @@ public class Reel : MonoBehaviour
     [SerializeField] private List<Sprite> spritesList = new List<Sprite>();
     [SerializeField] private float delay;
     [SerializeField] private float speed;
+    [SerializeField] private RectTransform rectTransform;
 
     private bool isFinal = false;
-    private Vector2 startPos;
+    private Vector3 startingPos = new Vector3(0, 1450, 0);
+    private Vector3 finalPos = new Vector3(0, 0, 0);
+    private Vector3 endingPos = new Vector3(0, -1450, 0);
 
-    public Vector2 StartPos { get => startPos; set => startPos = value; }
+    public Vector3 StartingPos => startingPos;
+    public Vector3 FinalPos => finalPos;
+    public Vector3 EndingPos => endingPos;
     public float Speed { get => speed; set => speed = value; }
 
     private void Start()
     {
-        //startPos = transform.position;
         SetItems();
+    }
+
+    public void SetPosition(Vector3 pos)
+    {
+        transform.localPosition = pos;
     }
 
     private void Update()
