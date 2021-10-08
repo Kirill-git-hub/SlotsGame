487bbc36dff633a72b2dcec824b68b302a8f20f0
Kirill_Vdovin <kirilvdovinkv@gmail.com>
2021-10-08 17:48:18 +0300

    - added a new variables to check order and method to destroy reel

--- New Unity Project/Assets/Scripts/Models/Reel.cs
+++ New Unity Project/Assets/Scripts/Models/Reel.cs
@@ -9,21 +9,33 @@ public class Reel : MonoBehaviour
     [SerializeField] private List<Sprite> spritesList = new List<Sprite>();
     [SerializeField] private float delay;
     [SerializeField] private float speed;
-    [SerializeField] private RectTransform rectTransform;
+    [SerializeField] private RectTransform reelRect;
+
+    [SerializeField] private bool isFisrt = false;
+    [SerializeField] private bool isCurrent = false;
+    [SerializeField] private bool isLast = false;
 
     private bool isFinal = false;
     private Vector3 startingPos = new Vector3(0, 1450, 0);
     private Vector3 finalPos = new Vector3(0, 0, 0);
     private Vector3 endingPos = new Vector3(0, -1450, 0);
+    private bool canSpin;
 
     public Vector3 StartingPos => startingPos;
     public Vector3 FinalPos => finalPos;
     public Vector3 EndingPos => endingPos;
     public float Speed { get => speed; set => speed = value; }
+    public float Delay => delay;
+
+    public bool IsFirst { get => isFisrt; set => isFisrt = value; }
+    public bool IsCurrent { get => isCurrent; set => isCurrent = value; }
+    public bool IsLast { get => isLast; set => isLast = value; }
+    public RectTransform ReelRect => reelRect;
 
     private void Start()
     {
         SetItems();
+        canSpin = false;
     }
 
     public void SetPosition(Vector3 pos)
@@ -33,7 +45,7 @@ public class Reel : MonoBehaviour
 
     private void Update()
     {
-        //RotateReel();
+        //RotateReel(100);
     }
 
     public void RotateReel(float reelSpeed)
@@ -47,5 +59,10 @@ public class Reel : MonoBehaviour
         {
             item.Init(spritesList[Random.Range(0, spritesList.Count)]);
         }
+    }
+
+    public void DestroyReel()
+    {
+        Destroy(gameObject);
     }
 }
