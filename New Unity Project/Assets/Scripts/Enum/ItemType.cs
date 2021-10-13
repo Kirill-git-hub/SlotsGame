c434252a61d7f3ba3eec407a71554c75bd07b910
Kirill_Vdovin <kirilvdovinkv@gmail.com>
2021-10-13 09:49:48 +0300

    - changed order and added index

--- New Unity Project/Assets/Scripts/Enum/ItemType.cs
+++ New Unity Project/Assets/Scripts/Enum/ItemType.cs
@@ -1,14 +1,14 @@
 ﻿
 public enum ItemType
 {
-    Strawberry,
-    Grapes,
-    Cherry,
-    Humburger,
-    Bell,
-    Coins,
-    Diamond,
-    Poop,
-    DogPooping,
-    BeardMan
+    BeardMan = 0,
+    Bell = 1,
+    Humburger = 2,
+    Cherry = 3,
+    Coins = 4,
+    Diamond = 5,
+    DogPooping = 6,
+    Grapes = 7,
+    Poop = 8,
+    Strawberry = 9,
 }
