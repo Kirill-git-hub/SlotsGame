f9e3bffb30bee4ac998c5838a47d8557bf4173eb
Kirill_Vdovin <kirilvdovinkv@gmail.com>
2021-10-13 09:49:12 +0300

    - added method to start spin

--- New Unity Project/Assets/Scripts/Controllers/GameController.cs
+++ New Unity Project/Assets/Scripts/Controllers/GameController.cs
@@ -4,9 +4,35 @@ using UnityEngine;
 
 public class GameController 
 {
+
     private GameView gameView;
     private SlotMachine slotMachine;
 
     public GameView GameView { get => gameView; set => gameView = value; }
     public SlotMachine SlotMachine { get => slotMachine; set => slotMachine = value; }
+
+    public void StartSpin()
+    {
+        MainApp.instance.GameController.SlotMachine.CanRotate();
+    }
+
+    //public void InitReels()
+    //{
+    //    //for(int i = 0; i < reels.Count; i++)
+    //    //{
+    //    //    reels[i].SpawnReels();
+    //    //}
+    //    //foreach (ReelSpawner r in reels)
+    //    //{
+    //    //    r.SpawnReels();
+    //    //}
+
+    //    for (int i = 0; i < SlotMachine.ReelContainer.Length; i++)
+    //    {
+    //        for (int j = 0; j < SlotMachine.ReelContainer[i].transform.childCount; j++)
+    //        {
+    //            //SlotMachine.ReelContainer[i].transform.GetChild(j).gameObject.
+    //        }
+    //    }
+    //}
 }
