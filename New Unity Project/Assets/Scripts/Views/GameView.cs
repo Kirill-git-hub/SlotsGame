1ab862a611e24e6f1b40a959ee5f9259d8e2432e
Kirill_Vdovin <kirilvdovinkv@gmail.com>
2021-10-22 17:46:12 +0300

    - added buttons and text for bet and lines, added listeners

--- New Unity Project/Assets/Scripts/Views/GameView.cs
+++ New Unity Project/Assets/Scripts/Views/GameView.cs
@@ -2,18 +2,31 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class GameView : MonoBehaviour
 {
     [SerializeField] private Button pauseButton = null;
     [SerializeField] private Button spinButton = null;
+    [SerializeField] private Button increaseBetButton = null;
+    [SerializeField] private Button decreaseBetButton = null;
+    [SerializeField] private Button increaseLinesCountButton = null;
+    [SerializeField] private Button decreaseLinesCountButton = null;
+    [SerializeField] private TextMeshProUGUI betText = null;
+    [SerializeField] private TextMeshProUGUI linesCountText = null;
+    [SerializeField] private TextMeshProUGUI totalWinText = null;
 
     public Button SpinButton { get => spinButton; set => spinButton = value; }
+    public TextMeshProUGUI LinesCountText { get => linesCountText; set => linesCountText = value; }
+    public TextMeshProUGUI BetText { get => betText; set => betText = value; }
+    public TextMeshProUGUI TotalWinText { get => totalWinText; set => totalWinText = value; }
 
     private void Start()
     {
         MainApp.instance.GameController.GameView = this;
         AddListener();
+        //MainApp.instance.GameController.UpdateLinesCount();
+
         Debug.Log("прикрепил ивент активировать панель паузы");
     }
 
@@ -22,6 +35,30 @@ public class GameView : MonoBehaviour
         pauseButton.onClick.AddListener(PopupController.instance.InstantiatePausePopup);
 
         SpinButton.onClick.AddListener(MainApp.instance.GameController.StartSpin);
+
+        increaseLinesCountButton.onClick.AddListener(() => 
+        {
+            MainApp.instance.GameController.IncreaseLinesCount();
+            MainApp.instance.GameController.UpdateLinesCount();
+        });
+
+        decreaseLinesCountButton.onClick.AddListener(() => 
+        {
+            MainApp.instance.GameController.DecreaseLinesCount();
+            MainApp.instance.GameController.UpdateLinesCount();
+        });
+
+        increaseBetButton.onClick.AddListener(() => 
+        {
+            MainApp.instance.GameController.IncreaseBet();
+            MainApp.instance.GameController.UpdateBet();
+        });
+
+        decreaseBetButton.onClick.AddListener(() => 
+        {
+            MainApp.instance.GameController.DecreaseBet();
+            MainApp.instance.GameController.UpdateBet();
+        });
     }
 
     public void RemoveListener()
