68df039fe3e55ec55981be76a94fd5e17db2c30d
Kirill_Vdovin <kirilvdovinkv@gmail.com>
2021-10-22 17:47:19 +0300

    - created methods to increase, decrease and update bet and lines

--- New Unity Project/Assets/Scripts/Controllers/GameController.cs
+++ New Unity Project/Assets/Scripts/Controllers/GameController.cs
@@ -15,4 +15,35 @@ public class GameController
     {
         MainApp.instance.GameController.SlotMachine.CanRotate();
     }
+
+    public void IncreaseBet()
+    {
+        MainApp.instance.GameController.SlotMachine.BetIndex++;
+    }
+
+    public void DecreaseBet()
+    {
+        MainApp.instance.GameController.SlotMachine.BetIndex--;
+    }
+
+    public void IncreaseLinesCount()
+    {
+        MainApp.instance.GameController.SlotMachine.LinesCount++;
+    }
+
+    public void DecreaseLinesCount()
+    {
+        MainApp.instance.GameController.SlotMachine.LinesCount--;
+    }
+
+    public void UpdateLinesCount()
+    {
+        MainApp.instance.GameController.GameView.LinesCountText.text = MainApp.instance.GameController.SlotMachine.LinesCount.ToString();
+    }
+
+    public void UpdateBet()
+    {
+        MainApp.instance.GameController.GameView.BetText.text = 
+            MainApp.instance.GameController.SlotMachine.Bet[MainApp.instance.GameController.SlotMachine.BetIndex].ToString();
+    }
 }
