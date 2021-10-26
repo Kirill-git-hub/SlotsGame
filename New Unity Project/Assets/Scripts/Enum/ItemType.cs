69dbc768ea53784d17ce3f2fdeb2591df5f1b26e
Kirill_Vdovin <kirilvdovinkv@gmail.com>
2021-10-26 15:48:56 +0300

    - deleted items type

--- New Unity Project/Assets/Scripts/Enum/ItemType.cs
+++ New Unity Project/Assets/Scripts/Enum/ItemType.cs
@@ -1,14 +1,10 @@
 ﻿
 public enum ItemType
 {
-    BeardMan = 0,
-    Bell = 1,
-    Burger = 2,
-    Cherry = 3,
-    Coins = 4,
-    Diamond = 5,
-    DogPooping = 6,
-    Grapes = 7,
-    Poop = 8,
-    Strawberry = 9,
+    Burger = 1,
+    Cherry = 2,
+    Coins = 3,
+    Diamond = 4,
+    Grapes = 5,
+    Poop = 6,
 }
