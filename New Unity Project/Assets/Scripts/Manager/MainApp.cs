876b7c50a1d2dfa3ac21ba2b91886fbbdbcea5df
Kirill_Vdovin <kirilvdovinkv@gmail.com>
2021-11-03 16:06:05 +0200

    - create controllers in awake method, invoke Update method in SlotMachine class

--- New Unity Project/Assets/Scripts/Manager/MainApp.cs
+++ New Unity Project/Assets/Scripts/Manager/MainApp.cs
@@ -1,33 +1,53 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MainApp : MonoBehaviour
 {
     public static MainApp instance = null;
+    private MainMenuController mainMenuController;
+    private GameController gameController;
+    private PopupController popupController;
+
+    public MainMenuController MainMenuController { get => mainMenuController; set => mainMenuController = value; }
+    public GameController GameController { get => gameController; set => gameController = value; }
+    public PopupController PopupController { get => popupController; set => popupController = value; }
+    //public GameObject MainMenu { get => mainMenu; set => mainMenu = value; }
 
     private void Awake()
     {
         if(instance == null)
-        {
-            instance = this;
-        }
-        else if (instance == this)
-        {
-            Destroy(gameObject);
-        }
+       {
+           instance = this;
+       }
+       else if (instance == this)
+       {
+           Destroy(gameObject);
+       }
 
-        DontDestroyOnLoad(gameObject);
+       DontDestroyOnLoad(gameObject);
+
+        PopupController = new PopupController();
+        GameController = new GameController();
+        MainMenuController = new MainMenuController();
     }
-
-    private MainMenuController mainMenuController;
-    private GameController gameController;
-
-    public MainMenuController MainMenuController { get => mainMenuController; set => mainMenuController = value; }
-    public GameController GameController { get => gameController; set => gameController = value; }
 
     private void Start()
     {
-        mainMenuController = new MainMenuController();
+        Debug.Log("Start in MainApp");
+        //MainMenuController.Start();
+        
+        //GameController.Start();
+    }
+
+    private void Update()
+    {
+        MainApp.instance.GameController.SlotMachine.Update();
+    }
+
+    public void InitializeGame()
+    {
+       GameController = new GameController();
     }
 }
