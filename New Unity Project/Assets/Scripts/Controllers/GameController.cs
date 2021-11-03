df67e8617cf5be27b930d54b1e6bd09517439a6a
Kirill_Vdovin <kirilvdovinkv@gmail.com>
2021-11-03 16:08:31 +0200

    - create object of type GameView and SlotMachine in constructor

--- New Unity Project/Assets/Scripts/Controllers/GameController.cs
+++ New Unity Project/Assets/Scripts/Controllers/GameController.cs
@@ -4,18 +4,35 @@ using UnityEngine;
 
 public class GameController 
 {
-
     private GameView gameView;
     private SlotMachine slotMachine;
+    private PopupController popupController;
 
     public GameView GameView { get => gameView; set => gameView = value; }
     public SlotMachine SlotMachine { get => slotMachine; set => slotMachine = value; }
+    public PopupController PopupController { get => popupController; set => popupController = value; }
+
+    public GameController()
+    {
+        SlotMachine = new SlotMachine();
+        GameView = new GameView();
+
+    }
+
+    public void Start()
+    {
+        Debug.Log("Start in GameController");
+
+        GameView.Start();
+    }
 
     public void StartSpin()
     {
         SlotMachine.CanRotate();
-        SlotMachine.TotalWin = 0f;
-        MainApp.instance.GameController.GameView.TotalWinText.text = SlotMachine.TotalWin.ToString();  
+        SlotMachine.TotalWin = 0f; //это нужное, не удалять
+        MainApp.instance.GameController.GameView.TotalWinText.text = SlotMachine.TotalWin.ToString(); 
+
+        Debug.Log("start spin method"); 
     }
 
     public void IncreaseBet()
