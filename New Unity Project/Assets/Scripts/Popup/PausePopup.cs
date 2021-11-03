676190814f0e6f3597c2a69a6a72055370613155
Kirill_Vdovin <kirilvdovinkv@gmail.com>
2021-11-03 16:31:10 +0200

    - find all needed objects in constructor, added listeners

--- New Unity Project/Assets/Scripts/Popup/PausePopup.cs
+++ New Unity Project/Assets/Scripts/Popup/PausePopup.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
@@ -7,20 +7,52 @@ using UnityEngine.Events;
 
 public class PausePopup : Popup
 {
-    [SerializeField] private Button continueButton = null;
-    [SerializeField] private Button exitButton = null;
+    //protected bool isActivePopup = true;
+    private Button continueButton = null;
+    private Button exitButton = null;
+    private GameObject pausePopupPanel;
+
+    public PausePopup()
+    {
+        pausePopupPanel = GameObject.Find("Canvas_Popups/PausePopup");
+        exitButton = pausePopupPanel.transform.Find("Panel_Pause/ButtonsContainer/Button_Exit").GetComponent<Button>();
+        continueButton = pausePopupPanel.transform.Find("Panel_Pause/ButtonsContainer/Button_ContinueGame").GetComponent<Button>();
+
+        continueButton.onClick.AddListener(() => DisactivatePopup(pausePopupPanel));
+        exitButton.onClick.AddListener(() => 
+        {
+            DisactivatePopup(pausePopupPanel);
+            MainApp.instance.GameController.GameView.DisactivateGamePanel();
+            MainApp.instance.MainMenuController.MainMenuView.ActivateMainMenuPanel();
+        });
+
+        //pausePopupPanel.SetActive(!isActivePopup);
+        DisactivatePopup(pausePopupPanel);
+    }
+
+    public void SetActivePausePopup()
+    {
+        pausePopupPanel.SetActive(isActivePopup);
+    }
+
+    // public void ClearRel()
+    // {
+    //     MainApp.instance.GameController.SlotMachine.ClearReels();
+    // }
 
     public void InstantiatePopup(UnityAction action, UnityAction action1)
     {
-        isInstantiatedCopy = true;
+        //isInstantiatedCopy = true;
+        //isActivePopup = true;
 
-        continueButton.onClick.AddListener(PopupController.instance.DeactivateActivePopup);
+        //continueButton.onClick.AddListener(PopupController.instance.DeactivateActivePopup);
+        //continueButton.onClick.AddListener(MainApp.instance.GameController.PopupController.DeactivateActivePopup);
 
-        exitButton.onClick.AddListener(()=>
-        {
-            SceneLoader.Unload(MainApp.instance.MainMenuController.MainMenuView.GameScene);
-            action();
-            action1();  
-        });
+        //exitButton.onClick.AddListener(()=>
+        //{
+        //    //SceneLoader.Unload(MainApp.instance.MainMenuController.MainMenuView.GameScene);
+        //    action();
+        //    action1();  
+        //});
     }
 }
