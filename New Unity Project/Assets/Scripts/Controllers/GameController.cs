10505ecc3afacf982525c44beff0bac38dc695a1
Kirill_Vdovin <kirilvdovinkv@gmail.com>
2021-11-04 10:49:19 +0200

    deleted start method and debugLog method

--- New Unity Project/Assets/Scripts/Controllers/GameController.cs
+++ New Unity Project/Assets/Scripts/Controllers/GameController.cs
@@ -6,11 +6,9 @@ public class GameController
 {
     private GameView gameView;
     private SlotMachine slotMachine;
-    private PopupController popupController;
 
     public GameView GameView { get => gameView; set => gameView = value; }
     public SlotMachine SlotMachine { get => slotMachine; set => slotMachine = value; }
-    public PopupController PopupController { get => popupController; set => popupController = value; }
 
     public GameController()
     {
@@ -18,20 +16,11 @@ public class GameController
         GameView = new GameView();
     }
 
-    public void Start()
-    {
-        Debug.Log("Start in GameController");
-
-        GameView.Start();
-    }
-
     public void StartSpin()
     {
         SlotMachine.CanRotate();
         SlotMachine.TotalWin = 0f;
         MainApp.instance.GameController.GameView.UpdateTotalWin();
-
-        Debug.Log("start spin method"); 
     }
 
     public void IncreaseBet()
