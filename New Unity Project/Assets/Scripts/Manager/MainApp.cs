8008f3ef491c48e99e3edcc56fc290d3ac5bf829
Kirill_Vdovin <kirilvdovinkv@gmail.com>
2021-11-04 10:48:12 +0200

    - deleted Start method

--- New Unity Project/Assets/Scripts/Manager/MainApp.cs
+++ New Unity Project/Assets/Scripts/Manager/MainApp.cs
@@ -13,7 +13,6 @@ public class MainApp : MonoBehaviour
     public MainMenuController MainMenuController { get => mainMenuController; set => mainMenuController = value; }
     public GameController GameController { get => gameController; set => gameController = value; }
     public PopupController PopupController { get => popupController; set => popupController = value; }
-    //public GameObject MainMenu { get => mainMenu; set => mainMenu = value; }
 
     private void Awake()
     {
@@ -31,14 +30,6 @@ public class MainApp : MonoBehaviour
         PopupController = new PopupController();
         GameController = new GameController();
         MainMenuController = new MainMenuController();
-    }
-
-    private void Start()
-    {
-        Debug.Log("Start in MainApp");
-        //MainMenuController.Start();
-        
-        //GameController.Start();
     }
 
     private void Update()
